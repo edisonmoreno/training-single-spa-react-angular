6152b1dd70c48d34d3976695589b98bb2530990b
Edison Moreno <john.moreno@sofka.com.co>
2021-10-18 21:31:48 -0500

    Registrar Micro-Frontends con Single-SPA

--- root-config-app/src/index.tsx
+++ root-config-app/src/index.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { LifeCycles, registerApplication, start } from "single-spa";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+registerApplication({
+  name: "app-angular",
+  app: (): Promise<LifeCycles> =>
+    (window as any).System.import("http://localhost:4200/main.js"),
+  activeWhen: "/angular",
+});
+
+registerApplication({
+  name: "app-react",
+  app: (): Promise<LifeCycles> =>
+    (window as any).System.import("@com-edisonmoreno/app-react"),
+  activeWhen: "/react",
+});
+
+start();
